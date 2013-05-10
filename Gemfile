91e70d1c4839b18a46e23e937bf988f69e622831
David Hu <octopie@gmail.com>
2013-05-10 17:54:46 -0400

    Add MongoDB support

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'mongo'
+gem 'bson'
+gem 'bson_ext'
 
 group :development do
   gem 'foreman'
--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
+include Mongo
+
+mongo = MongoClient.new('localhost', 27017)
+
 get '/' do
+	# mongodb usage -- see README for proper db setup
+	db = mongo['test']
+	logger.info(db['testCollection'].find({'foo' => 'bar'}).first)
+
 	erb :index
 end
