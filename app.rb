f632353c2f2b696790838d1e83b4c4d8eb031ef6
David Hu <octopie@gmail.com>
2013-05-13 21:26:44 -0400

    More mongo-heroku integration fixes

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 include Mongo
 
+# mongodb setup -- see README for more
 mongo_uri = ENV['MONGOLAB_URI'] || 'mongodb://localhost:27017'
-
 mongo = MongoClient.from_uri(mongo_uri)
+db = mongo[mongo_uri[%r{/([^/\?]+)(\?|$)}, 1] || 'test']
+
 
 get '/' do
-	# mongodb usage -- see README for proper db setup
-	db = mongo['test']
+	# sanity check for mongo: a document with this needs to exist
 	logger.info(db['testCollection'].find({'foo' => 'bar'}).first)
 
 	erb :index
