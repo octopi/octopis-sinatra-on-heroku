5789dfbccacbeeb27071a098246a27f75290fa59
David Hu <octopie@gmail.com>
2013-05-13 22:12:15 -0400

    Fix local mongo config

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ include Mongo
 # mongodb setup -- see README for more
 mongo_uri = ENV['MONGOLAB_URI'] || 'mongodb://localhost:27017'
 mongo = MongoClient.from_uri(mongo_uri)
-db = mongo[mongo_uri[%r{/([^/\?]+)(\?|$)}, 1] || 'test']
-
+db = mongo[mongo_uri.include?('mongolab') ? mongo_uri[%r{/([^/\?]+)(\?|$)}, 1] : 'test']
 
 get '/' do
 	# sanity check for mongo: a document with this needs to exist
